6f4b2a2025696da5caac86509fbd3da8ec291f24
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-13 14:02:21 -0600

    Complete 'The Thing Namer 3000', pg. 31

---
+++ Part 1/4 Comments/TheThingNamer3000/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+// noun
+string noun = Console.ReadLine();
+
+/* prefix */
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string prefix = Console.ReadLine();
+
+string suffix1 = "of Doom"; // first suffix
+
+string suffix2 = "3000"; // second suffix
+
+Console.WriteLine($"The {prefix} {noun} {suffix1} {suffix2}!");
+
+// Answer: You could make this code more readable by changing the variable names to reflect their purpose.
+// This would mostly make the comments redundant, which is fine.
+// Using string interpolation might(?) make bugs like the duplicate "of" bug easier to spot and avoid.
+// Adding empty lines between different parts of the name helps, or at least looks nice to me.
+// But this isn't a very complicated program so these steps are not very necessary...
