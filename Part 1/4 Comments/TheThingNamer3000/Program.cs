9563cb948f3e592ce1f99fff3d7cb6ec3bb49ae8
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-13 14:02:21 -0600

    Complete 'The Thing Namer 3000', pg. 31

---
+++ Part 1/4 Comments/TheThingNamer3000/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+// noun
+string a = Console.ReadLine();
+
+/* prefix */
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine();
+
+string c = "of Doom"; // first suffix
+string d = "3000"; // second suffix
+Console.WriteLine("The " + b + " " + a + " " + c + " " + d + "!");
