6b13bad6ef5319a5c388daab087e331a293a05ff
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-13 13:26:10 -0600

    Complete 'The Makings of a Programmer', pg. 24

--- Part 1/3 Hello World/Program.cs
+++ Part 1/3 Hello World/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine($"Hello, World! Version {1+1}!");
+Console.WriteLine("The number of statements a program can contain is limitless for most purposes.");
+Console.WriteLine("If it really matters I guess it's limited by the hardware running the program.");
+Console.WriteLine("But that's not going to be a concern outside of exceptionally weak devices.");
+Console.WriteLine("There are types of programming where it's necessary to work with very weak " +
+    "hardware, but I would guess that even in those cases other hardware limitations are usually " +
+    "more important than the amount of memory available to store statements.");
