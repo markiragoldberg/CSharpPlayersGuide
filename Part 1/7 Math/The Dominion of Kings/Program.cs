51e004274828c0469a016691193c605fe77b7cb0
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-14 14:51:38 -0600

    Add 'The Dominion of Kings', pg. 57

---
+++ Part 1/7 Math/The Dominion of Kings/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter the total number of provinces you own: ");
+int provinces = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Do the same for duchies: ");
+int duchies = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Finally do the same for estates: ");
+int estates = Convert.ToInt32(Console.ReadLine());
+
+int points = provinces * 6 + duchies * 3 + estates;
+
+Console.WriteLine($"Your holdings are worth {points} points.");
