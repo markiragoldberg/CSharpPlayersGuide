6fb167f3adba97e8f4b050cc49fdfaa78d3b6131
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-14 14:47:41 -0600

    Complete 'The Triangle Farmer', pg. 53

---
+++ Part 1/7 Math/The Triangle Farmer/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter the length of the triangle's base: ");
+double baseLength = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the triangle's height': ");
+double height = Convert.ToDouble(Console.ReadLine());
+
+double area = (baseLength * height) / 2.0;
+
+Console.WriteLine($"The triangle's area is {area}.");
