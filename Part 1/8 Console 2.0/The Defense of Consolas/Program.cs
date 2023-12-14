0e79ea31152478b70a8c3eb5f05318729b62d9b4
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-14 14:52:18 -0600

    Add 'The Defense of Consolas', pg. 68

---
+++ Part 1/8 Console 2.0/The Defense of Consolas/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Title = "The Defense of Consolas";
+
+Console.Write("Target row? ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Target column? ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Deploy to:");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine($"({row},{column-1})");
+Console.WriteLine($"({row-1},{column})");
+Console.WriteLine($"({row},{column+1})");
+Console.WriteLine($"({row+1},{column})");
+Console.ForegroundColor = ConsoleColor.White;
+Console.Beep();
