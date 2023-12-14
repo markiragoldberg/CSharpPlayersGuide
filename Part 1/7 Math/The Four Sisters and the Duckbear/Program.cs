9a42c49a82f95c38f80601371ed5b0782ef57c5b
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-14 14:50:13 -0600

    Add 'The Four Sisters and the Duckbear', pg. 56

---
+++ Part 1/7 Math/The Four Sisters and the Duckbear/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter the number of chocolate eggs collected today: ");
+int total_eggs = Convert.ToInt32(Console.ReadLine());
+int eggs_per_sister = total_eggs / 4; // 4 sisters
+int eggs_for_duckbear = total_eggs % 4;
+Console.WriteLine(
+    $"Each sister should get {eggs_per_sister} eggs, " +
+    $"and the duckbear should get {eggs_for_duckbear} eggs.");
+
+// The duckbear gets more eggs than any one sister if they collect one to three eggs,
+// or six to seven eggs, or eleven eggs.
