2d036a9c8626c81eeae4d448ca08e691e236b36c
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-16 01:01:05 -0600

    Complete 'The Prototype', pg. 88-89

---
+++ Part 1/11 Looping/The Prototype/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("User 1, enter a number between 0 and 100: ");
+int target_number = Convert.ToInt32(Console.ReadLine());
+while (target_number < 0 || target_number > 100)
+{
+    Console.Write("Sorry, the number must be between 0 and 100: ");
+    target_number = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Clear();
+
+Console.WriteLine("User 2, guess the number.");
+int guess;
+while (true)
+{
+    Console.Write("What is your next guess? ");
+    guess = Convert.ToInt32(Console.ReadLine());
+    if (guess == target_number)
+        break;
+    Console.WriteLine($"{guess} is too {(guess > target_number ? "high" : "low")}.");
+}
+Console.WriteLine("You guessed the number!");
