c7f10567333f653058a9cd30d163889548331a38
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-15 23:52:28 -0600

    Complete 'Watchtower', pg. 78

---
+++ Part 1/9 Decision Making/Watchtower/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.Write("What is the enemy's x-coordinate, relative to the watchtower? ");
+int enemy_x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("And what is their y-coordinate? ");
+int enemy_y = Convert.ToInt32(Console.ReadLine());
+
+if(enemy_x < 0)
+{
+    if(enemy_y < 0)
+    {
+        Console.WriteLine("The enemy is to the southwest!");
+    }
+    else if(enemy_y > 0)
+    {
+        Console.WriteLine("The enemy is to the northwest!");
+    }
+    else
+    {
+        Console.WriteLine("The enemy is to the west!");
+    }
+}
+else if(enemy_x > 0)
+{
+    if (enemy_y < 0)
+    {
+        Console.WriteLine("The enemy is to the southeast!");
+    }
+    else if (enemy_y > 0)
+    {
+        Console.WriteLine("The enemy is to the northeast!");
+    }
+    else
+    {
+        Console.WriteLine("The enemy is to the east!");
+    }
+}
+else
+{
+    if (enemy_y < 0)
+    {
+        Console.WriteLine("The enemy is to the south!");
+    }
+    else if (enemy_y > 0)
+    {
+        Console.WriteLine("The enemy is to the north!");
+    }
+    else
+    {
+        Console.WriteLine("The enemy is here!");
+    }
+}
