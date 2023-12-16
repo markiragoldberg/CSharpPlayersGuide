089f923e5356b0767caf9721feb7d457bdaf94c4
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-16 01:13:52 -0600

    Complete 'The Magic Cannon', pg. 89

---
+++ Part 1/11 Looping/The Magic Cannon/Program.cs
@@ -0,0 +1,25 @@
+﻿for(int i = 1; i < 101; i++)
+{
+    Console.Write($"{i}: ");
+    string shot = "Normal";
+    if (i % 3 == 0)
+    {
+        if (i % 5 == 0)
+        {
+            shot = "Combined Fire/Electric";
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+        else
+        {
+            shot = "Fire";
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+    }
+    else if (i % 5 == 0)
+    {
+        shot = "Electric";
+        Console.ForegroundColor = ConsoleColor.Yellow;
+    }
+    Console.WriteLine(shot);
+    Console.ForegroundColor = ConsoleColor.Gray;
+}
