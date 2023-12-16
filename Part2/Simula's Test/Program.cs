b0d937f2b540ca8b0ababc65b21efd9901e561e8
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-16 15:27:54 -0600

    Complete "Simula's Test", pg. 135

---
+++ Part2/Simula's Test/Program.cs
@@ -0,0 +1,43 @@
+﻿ChestState currentState = ChestState.Locked;
+
+while(true)
+{
+    Console.Write($"The chest is {currentState.ToString().ToLower()}. What do you want to do? ");
+    string response = Console.ReadLine();
+    switch (response)
+    {
+        case "unlock":
+            if (currentState == ChestState.Locked)
+            {
+                currentState = ChestState.Closed;
+            }
+            break;
+        case "open":
+            if (currentState == ChestState.Closed)
+            {
+                currentState = ChestState.Open;
+            }
+            break;
+        case "lock":
+            if (currentState == ChestState.Closed)
+            {
+                currentState = ChestState.Locked;
+            }
+            break;
+        case "close":
+            if (currentState == ChestState.Open)
+            {
+                currentState = ChestState.Closed;
+            }
+            break;
+        case "quit":
+        case "exit":
+            return;
+        default:
+            break;
+    }
+}
+
+// -------------------------------------
+
+enum ChestState { Locked, Closed, Open }
