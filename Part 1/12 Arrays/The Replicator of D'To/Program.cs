0270024c9414ae9dd49ece69ea4e6416a31c0305
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-16 01:58:02 -0600

    Complete "The Replicator of D'To", pg. 94

---
+++ Part 1/12 Arrays/The Replicator of D'To/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Provide the first array.");
+int[] array = new int[5];
+for(int i = 0; i < 5; i++)
+{
+    Console.Write($"Enter the value at index {i}: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("\nReplicating the array...");
+int[] array2 = new int[5];
+for(int i = 0;i < array.Length; i++)
+{
+    array2[i] = array[i];
+}
+
+Console.Write("Array 1: \n[");
+for (int i = 0; i < array.Length-1; i++)
+{
+    Console.Write($"{array[i]}, ");
+}
+Console.WriteLine($"{array[^1]}]");
+
+Console.Write("\nArray 2: \n[");
+for (int i = 0; i < array2.Length-1; i++)
+{
+    Console.Write($"{array2[i]}, ");
+}
+Console.WriteLine($"{array2[^1]}]");
