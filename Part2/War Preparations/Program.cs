f5a1c7ffa4e194705de16870e9313daa31520cf6
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-24 14:32:24 -0600

    Complete 'War Preparations', pg. 231

---
+++ Part2/War Preparations/Program.cs
@@ -0,0 +1,15 @@
+﻿Sword basic = Sword.CreateBasic();
+Sword gladius = basic with { Length = 60f, CrossguardWidth = 7f };
+Sword wizsword = basic with { Material = Material.Bronze, Gemstone = Gemstone.Emerald };
+Console.WriteLine(basic);
+Console.WriteLine(gladius);
+Console.WriteLine(wizsword);
+// -----------------------------------
+
+public record Sword(Material Material, Gemstone? Gemstone, float Length, float CrossguardWidth)
+{
+    public static Sword CreateBasic() => new(Material.Iron, null, 80f, 16f);
+}
+
+public enum Material { Wood, Bronze, Iron, Steel, Binarium }
+public enum Gemstone { Emerald, Amber, Sapphire, Diamond, Bitstone }
