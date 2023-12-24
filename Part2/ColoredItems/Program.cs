4fe0b71c113fcdda1325d75b6d0612d84014604b
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-24 14:58:55 -0600

    Complete 'Colored Items', pg. 240

---
+++ Part2/ColoredItems/Program.cs
@@ -0,0 +1,31 @@
+﻿ColoredItem<Sword> greenSword = new(ConsoleColor.Green, new Sword());
+ColoredItem<Bow> redBow = new(ConsoleColor.Red, new Bow());
+ColoredItem<Axe> blueAxe = new(ConsoleColor.Blue, new Axe());
+Console.Write("I have a ");
+greenSword.Display();
+Console.Write(", a ");
+redBow.Display();
+Console.Write(", and an ");
+blueAxe.Display();
+Console.WriteLine(".");
+
+// --------------------------------
+public class ColoredItem<T>
+{
+    public ConsoleColor Color { get; set; }
+    public T Item { get; }
+    public ColoredItem(ConsoleColor color, T item)
+    {
+        Color = color;
+        Item = item;
+    }
+    public void Display()
+    {
+        Console.ForegroundColor = Color;
+        Console.Write(Item);
+        Console.ResetColor();
+    }
+}
+public class Sword { }
+public class Bow { }
+public class Axe { }
