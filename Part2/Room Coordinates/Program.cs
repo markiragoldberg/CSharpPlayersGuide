4b367d9a5e7f9847488caf3f78570bb465df5b79
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-24 13:54:13 -0600

    Complete 'Room Coordinates', pg. 225-226

---
+++ Part2/Room Coordinates/Program.cs
@@ -0,0 +1,46 @@
+﻿ReportAdjacency(0, 0);
+ReportAdjacency(0, 1);
+ReportAdjacency(0, -1);
+ReportAdjacency(1, 0);
+ReportAdjacency(-1, 0);
+ReportAdjacency(-1, -1);
+ReportAdjacency(-1, 1);
+ReportAdjacency(1, -1);
+ReportAdjacency(1, 1);
+ReportAdjacency(-2, 0);
+ReportAdjacency(2, 0);
+ReportAdjacency(0, 2);
+ReportAdjacency(0, -2);
+
+// ----------------------------------
+static void ReportAdjacency(int X = 0, int Y = 0)
+{
+    Coordinate origin = new();
+    Coordinate other = new(X, Y);
+    Console.WriteLine($"Origin is {(origin.IsAdjacent(other) ? "" : "not ")}" +
+        $"adjacent to {other}.");
+}
+
+// ----------------------------------
+
+readonly struct Coordinate
+{
+    public int X { get; init; }
+    public int Y { get; init; }
+
+    public Coordinate(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    public readonly bool IsAdjacent(Coordinate other)
+    {
+        int x_diff = Math.Abs(X - other.X);
+        int y_diff = Math.Abs(Y - other.Y);
+        return (x_diff == 1 && y_diff == 0) || (x_diff == 0 && y_diff == 1);
+    }
+    public readonly override string ToString()
+    {
+        return $"({X,2},{Y,2})";
+    }
+}
