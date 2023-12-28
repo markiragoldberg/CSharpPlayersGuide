483ff5a03089ca0f8ff2bdb94cb42a1d02832755
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-28 11:26:41 -0600

    Complete 'Better Random', pg. 278

---
+++ Part3/Better Random/Program.cs
@@ -0,0 +1,19 @@
+﻿using BetterRandom;
+
+Random rng = new Random();
+Console.WriteLine($"Doubles less than 10.0: ");
+for (int i = 0; i < 15; i++)
+    Console.Write($"{rng.NextDouble(10.0):0.0} ");
+
+Console.WriteLine($"\nStrings from up, down, left, right:");
+string[] directions = {"up", "down", "left", "right"};
+for (int i = 0; i < 12; i++)
+    Console.Write($"{rng.NextString(directions)} ");
+
+Console.WriteLine($"\nCoinflips:");
+for (int i = 0; i < 40; i++)
+    Console.Write($"{(rng.CoinFlip() == true ? "1" : "0")} ");
+
+Console.WriteLine($"\nCoinflips with 85% chance of 0:");
+for (int i = 0; i < 40; i++)
+    Console.Write($"{(rng.CoinFlip(0.15) == true ? "1" : "0")} ");
---
+++ Part3/Better Random/RandomExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace BetterRandom
+{
+    public static class RandomExtensions
+    {
+        public static double NextDouble(this Random self, double maximum)
+        {
+            return self.NextDouble() * maximum;
+        }
+
+        public static string? NextString(this Random self, params string[] options)
+        {
+            if (options.Length > 0)
+                return options[self.Next(options.Length)];
+            return null;
+        }
+
+        public static bool CoinFlip(this Random self, double trueFrequency = 0.5)
+        {
+            return self.NextDouble() < trueFrequency;
+        }
+    }
+}
