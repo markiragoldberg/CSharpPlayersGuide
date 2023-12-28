fce0985ce66ac7d12b737e25429d8f46e892c95b
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-28 13:02:25 -0600

    Enhance Parser for use in "Exepti's Game"

--- Part3/Safer Number Crunching/Parser.cs
+++ Part3/Safer Number Crunching/Parser.cs
@@ -1,4 +1,6 @@
-﻿namespace SaferNumberCrunching
+﻿using System.Numerics;
+
+namespace SaferNumberCrunching
 {
     public static class Parser
     {
@@ -11,6 +13,63 @@
                 if (T.TryParse(input, null, out T? result))
                 {
                     return result;
+                }
+                else
+                {
+                    Console.Write(repeatPrompt ?? "Try again: ");
+                }
+            }
+        }
+        public static T AskForTypeInRange<T>(
+            string prompt, T min, T max, string? repeatPrompt = null, string? outOfRangePrompt = null)
+            where T : IParsable<T>, IComparisonOperators<T, T, bool>
+        {
+            Console.Write(prompt);
+            while (true)
+            {
+                string? input = Console.ReadLine();
+                if (T.TryParse(input, null, out T? result))
+                {
+                    if (min <= result && result <= max)
+                    {
+                        return result;
+                    }
+                    else
+                    {
+                        Console.Write(outOfRangePrompt ?? "That is out of range. Try again: ");
+                    }
+                }
+                else
+                {
+                    Console.Write(repeatPrompt ?? "Try again: ");
+                }
+            }
+        }
+        public static T AskForTypeInRangeWithExclusions<T>(
+            string prompt, T min, T max, List<T> exclusions, string? repeatPrompt = null, string? outOfRangePrompt = null, string? excludedPrompt = null)
+            where T : IParsable<T>, IComparisonOperators<T, T, bool>
+        {
+            Console.Write(prompt);
+            while (true)
+            {
+                string? input = Console.ReadLine();
+                if (T.TryParse(input, null, out T? result))
+                {
+                    if (min <= result && result <= max)
+                    {
+                        if (!exclusions.Contains(result))
+                        {
+                            return result;
+                        }
+                        else
+                        {
+                            Console.Write(excludedPrompt ?? "That is excluded from the range. Try again: ");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write(outOfRangePrompt ?? "That is out of range. Try again: ");
+                    }
                 }
                 else
                 {
