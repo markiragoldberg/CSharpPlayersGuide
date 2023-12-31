0d7eb05fc82a6344ee856a326878a92ae9a0c4d4
Mark Ira Goldberg <markiragoldberg@outlook.com>
2023-12-30 20:28:36 -0600

    Complete 'Indexing Operand City', pg. 332

--- Part3/Navigating Operand City/Program.cs
+++ Part3/Navigating Operand City/Program.cs
@@ -10,6 +10,8 @@ BlockOffset offset = new(3, 6);
 Console.WriteLine($"Moving via {offset}");
 bc += offset;
 Console.WriteLine(bc);
+Console.WriteLine($"bc[0]: {bc[0]}");
+Console.WriteLine($"bc[1]: {bc[1]}");
 offset = new(-3, -6);
 Console.WriteLine($"Returning via {offset}");
 bc += offset;
@@ -36,6 +38,22 @@ public record BlockCoordinate(int Row, int Column)
             default:
                 return new BlockCoordinate(coord.Row, coord.Column-1);
         }
+    }
+    public int this[int index]
+    {
+        get
+        {
+            if (index == 0) return Row;
+            else if (index == 1) return Column;
+            else throw new IndexOutOfRangeException();
+        }
+
+        // I do not think the indexer is a good addition to the BlockCoordinate 
+        // unless it is used extremely heavily and needs extremely concise syntax.
+        // Otherwise you are simply inviting confusion over which index is the 
+        // row and which is the column. If the user is more familiar with 
+        // the form (x, y) where x = col, y = row then their intuition will be
+        // opposite of how the BlockCoordinate is supposed to be used.
     }
 }
 public record BlockOffset(int RowOffset, int ColumnOffset);
