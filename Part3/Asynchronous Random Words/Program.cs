58f8644c8b2b0a46dfe06bd95f1063f7a68c3369
Mark Ira Goldberg <markiragoldberg@outlook.com>
2024-01-03 12:39:13 -0600

    Complete '... Random Words', pg. 359-360 Both 'Asynchronous Random Words' and 'Many Random Words'.

---
+++ Part3/Asynchronous Random Words/Program.cs
@@ -0,0 +1,63 @@
+﻿string? word = null;
+while (word != "quit")
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    if (word != null)
+    {
+        RandomlyRecreateAndReportAsync(word);
+        Console.Write("Working on ");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write(word);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("...");        
+    }
+    Console.Write("What word do you want to recreate randomly? ");
+    Console.ForegroundColor = ConsoleColor.Cyan;
+
+    word = Console.ReadLine();
+}
+Console.ResetColor();
+
+// -------------------------
+async void RandomlyRecreateAndReportAsync(string word)
+{
+    DateTime start = DateTime.Now;
+    Task<int> attempts = Task.Run(() => RandomlyRecreate(word));
+    await attempts;
+    TimeSpan time = DateTime.Now - start;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write($"\nIt took ");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write($"{attempts.Result} attempts");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write(" and ");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write($"{time.TotalSeconds:0.0} seconds");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write(" to randomly recreate the word ");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.Write(word);
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine(".");
+    Console.ForegroundColor = ConsoleColor.Cyan;
+}
+Task<int> RandomlyRecreateAsync(string word)
+    => Task.Run(() => RandomlyRecreate(word));
+
+int RandomlyRecreate(string word)
+{
+    Random rng = new Random();
+    int length = word.Length;
+    System.Text.StringBuilder randomWord = new();
+    int attempts = 0;
+    while (randomWord.ToString() != word)
+    {
+        attempts += 1;
+        randomWord.Clear();
+        for (int i = 0; i < length; i++)
+        {
+            randomWord.Append((char)('a' + rng.Next(26)));
+        }
+    }
+    return attempts;
+}
