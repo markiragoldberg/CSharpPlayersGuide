b7bfff5cb30e47330d7c89eb820a03b5bfb9ff5d
Mark Ira Goldberg <markiragoldberg@outlook.com>
2024-01-04 01:39:42 -0600

    Port 'The Robot Factory' to ConsoleIO

--- Part3/The Robot Factory/Program.cs
+++ Part3/The Robot Factory/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System.Dynamic;
-using PlayersGuide;
+using ConsoleIO;
 
 int nextID = 1;
 while (true)
@@ -9,16 +9,16 @@ while (true)
     Console.WriteLine($"You are producing robot #{robot.id}.");
 
     // Define robot
-    if (ConsoleInput.AskToConfirm("Do you want to name this robot? "))
-        robot.name = ConsoleInput.AskForType<string>("What is its name? ");
+    if (ColoredConsole.AskToConfirm("Do you want to name this robot? "))
+        robot.name = ColoredConsole.AskForType<string>("What is its name? ");
 
-    if (ConsoleInput.AskToConfirm("Does this robot have a specific size? "))
+    if (ColoredConsole.AskToConfirm("Does this robot have a specific size? "))
     {
-        robot.height = ConsoleInput.AskForType<double>("What is the robot's height? ");
-        robot.width = ConsoleInput.AskForType<double>("What is the robot's width? ");
+        robot.height = ColoredConsole.AskForType<double>("What is the robot's height? ");
+        robot.width = ColoredConsole.AskForType<double>("What is the robot's width? ");
     }
-    if (ConsoleInput.AskToConfirm("Does this robot need to be a specific color? "))
-        robot.color = ConsoleInput.AskForType<string>("What color? ");
+    if (ColoredConsole.AskToConfirm("Does this robot need to be a specific color? "))
+        robot.color = ColoredConsole.AskForType<string>("What color? ");
 
     // Display robot
     foreach (KeyValuePair<string, object> property in (IDictionary<string, object>)robot)
