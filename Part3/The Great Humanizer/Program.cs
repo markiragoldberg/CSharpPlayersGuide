9521c9d2a8afd8a53e77f440b7e14751a0b76197
Mark Ira Goldberg <markiragoldberg@outlook.com>
2024-01-04 02:17:55 -0600

    Complete 'The Great Humanizer', pg. 398

---
+++ Part3/The Great Humanizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using Humanizer;
+
+(DateOnly dateNow, TimeOnly timeNow) = DateTime.Now;
+TimeOnly feastTime = new(18, 00);
+DateOnly dailyFeastDate = timeNow <= feastTime ? dateNow : dateNow.AddDays(1);
+DateTime nextDailyFeast = new(dailyFeastDate, feastTime, DateTimeKind.Local);
+Console.WriteLine($"When is the daily feast? {nextDailyFeast.Humanize()}");
+
+DateTime newYearFeast = new(dateNow.Year, 12, 31, 11, 59, 59, 999, 999, DateTimeKind.Local);
+Console.WriteLine($"When is the New Year feast? {newYearFeast.Humanize()}");
+
+Console.WriteLine($"When is the feast fifty hours from now? {DateTime.Now.AddHours(50).Humanize()}");
