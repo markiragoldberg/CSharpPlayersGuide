6e1f75da7ea0875c6dfd6e90b22350332d3b409b
Mark Ira Goldberg <markiragoldberg@outlook.com>
2024-01-23 16:01:30 -0600

    Allow inverting MessageCategories

--- Part4/The Final Battle/Messaging/Message.cs
+++ Part4/The Final Battle/Messaging/Message.cs
@@ -4,6 +4,18 @@
 	{
 		public string Text { get => text; }
 		public MessageCategory Category { get => category; }
+
+		public static void InvertCategory(ref MessageCategory category)
+		{
+			category = category switch
+			{
+				MessageCategory.Good => MessageCategory.Bad,
+				MessageCategory.Bad => MessageCategory.Good,
+				MessageCategory.VeryGood => MessageCategory.VeryBad,
+				MessageCategory.VeryBad => MessageCategory.VeryGood,
+				_ => category
+			};
+		}
 	}
 	public enum MessageCategory
 	{
