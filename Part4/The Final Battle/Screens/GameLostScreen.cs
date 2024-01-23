656a3a59764a22214241bddf7517d4464d313082
Mark Ira Goldberg <markiragoldberg@outlook.com>
2024-01-23 15:53:51 -0600

    Create win and lose screens

---
+++ Part4/The Final Battle/Screens/GameLostScreen.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleIO;
+
+namespace The_Final_Battle.Screens
+{
+	public static class GameLostScreen
+	{
+		public static void Display()
+		{
+			ColoredConsole.WriteLine("The heroes have fallen! The Uncoded One has prevailed...", ConsoleColor.Magenta);
+		}
+	}
+}
---
+++ Part4/The Final Battle/Screens/GameWonScreen.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleIO;
+
+namespace The_Final_Battle.Screens
+{
+	public static class GameWonScreen
+	{
+		public static void Display()
+		{
+			ColoredConsole.WriteLine("The Uncoded One has been defeated! The Realms of C# are saved!", ConsoleColor.Green);
+		}
+	}
+}
