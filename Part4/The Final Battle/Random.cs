1ca6f66c1b580d7faf205fbc044c7453a283059c
Mark Ira Goldberg <markiragoldberg@outlook.com>
2024-01-31 21:34:09 -0600

    Cleanup Random.cs

--- Part4/The Final Battle/Random.cs
+++ Part4/The Final Battle/Random.cs
@@ -1,37 +1,37 @@
-﻿    namespace The_Final_Battle
+﻿namespace The_Final_Battle
 {
-    public static class RNG
-    {
-        public readonly static System.Random rng = new();
+	public static class RNG
+	{
+		public readonly static System.Random rng = new();
 
-        public static int Roll(int min, int max, int bonus) =>
-            rng.Next(min, max+1) + bonus;
+		public static int Roll(int min, int max, int bonus) =>
+			rng.Next(min, max + 1) + bonus;
 
-        public static bool PercentChance(double chance) =>
-            rng.NextDouble() < chance;
+		public static bool PercentChance(double chance) =>
+			rng.NextDouble() < chance;
 
-        public static T RandomElement<T>(this IList<T> list)
-        {
-            return list.ElementAt(rng.Next(0, list.Count));
-        }
-    }
+		public static T RandomElement<T>(this IList<T> list)
+		{
+			return list.ElementAt(rng.Next(0, list.Count));
+		}
+	}
 
-    public interface IDice<T>
-    {
-        public T Roll();
-    }
+	public interface IDice<T>
+	{
+		public T Roll();
+	}
 
 	public class Dice : IDice<int>
 	{
 		public int MinDamage { get; }
 		public int MaxDamage { get; }
 		public int BonusDamage { get; }
-        public Dice(int minDamage, int maxDamage, int bonusDamage=0)
-        {
-            MinDamage = minDamage;
-            MaxDamage = maxDamage;
-            BonusDamage = bonusDamage;
-        }
-        public int Roll() => RNG.Roll(MinDamage, MaxDamage, BonusDamage);
+		public Dice(int minDamage, int maxDamage, int bonusDamage = 0)
+		{
+			MinDamage = minDamage;
+			MaxDamage = maxDamage;
+			BonusDamage = bonusDamage;
+		}
+		public int Roll() => RNG.Roll(MinDamage, MaxDamage, BonusDamage);
 	}
 }
